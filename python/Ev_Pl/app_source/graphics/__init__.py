5ca3c137e8a60e2aca3bbaf11a983be06a2cd2cd
Alex Rosegrid <rosegrid58@gmail.com>
2023-02-16 20:21:11 +0530

    .

--- python/Ev_Pl/app_source/graphics/__init__.py
+++
@@ -1,6 +0,0 @@
-from app_source.graphics.main_frame import Mainframe
-from app_source.graphics.right_frame import Rightframe
-from app_source.graphics.tab_frame import TabsFrame
-from app_source.graphics.frame_resources.toplevels import *
-from app_source.graphics.frame_resources.specials import TitleBar
-import app_source.graphics.frame_resources.inner_frames as inner
