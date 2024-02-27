269c842a111cab0bad95f56e451f44eb8bcfc01e
Alex Rosegrid <rosegrid58@gmail.com>
2024-02-27 20:56:02 +0530

    .

--- clj/guess-num.clj
+++ clj/guess-num.clj
@@ -1,18 +1,21 @@
 (println "Guess the Number!")
 
+(defn input []
+  (print "Enter any number: ")
+  (flush)
+  (Integer/parseInt (read-line)))
+
 (let [number (rand-int 100)]
-  (defn input []
-    (print "Enter any number: ")
-    (flush)
-    (Integer/parseInt (read-line)))
   (defn check [guess]
     (cond
-      (> guess number) (do
-                         (println "Too High...")
-                         false)
-      (< guess number) (do
-                         (println "...Too Low")
-                         false)
+      (> guess number)
+      (do
+        (println "Too High...")
+        false)
+      (< guess number)
+      (do
+        (println "...Too Low")
+        false)
       :else true))
   (while (not (check (input))))
   (println "You Got It!"))
