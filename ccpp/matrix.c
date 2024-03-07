612f2fa24c0c399feabb8acbfcdfc9b96284174d
Alex Rosegrid <rosegrid58@gmail.com>
2024-03-07 20:53:16 +0530

    .

--- ccpp/matrix.c
+++ ccpp/matrix.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define INPUT_BUFFER_SIZE 128
 
 void matrix_add(int rows, int cols, int matrix_a[rows][cols], int matrix_b[rows][cols], int matrix_c[rows][cols]) {
     for (int i = 0; i < rows; ++i) {
@@ -21,10 +25,57 @@ void matrix_transpose(int rows, int cols, int matrix[rows][cols]) {
 void matrix_print(int rows, int cols, int matrix[rows][cols]) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            printf("%d ", matrix[i][j]);
+            printf("%4d ", matrix[i][j]);
         }
         putchar('\n');
     }
+}
+
+static inline void print_row_prompt(int row, int cols) {
+    printf("Enter %d row elements", row);
+    // Display required indices
+    for (int i = 0; i < cols; ++i) {
+        printf("[%d %d] ", row, i);
+    }
+    printf("\n>> ");
+}
+
+static inline int parse_row_input(char *buf, int row, int cols, int matrix[][cols]) {
+    buf[strcspn(buf, "\n")] = '\0';
+    char *split = strtok(buf, " ");
+    char *end;
+    int j = 0;
+    while (split) {
+        if (j >= cols) {
+            fputs("WARNING: Input has more elements than required; turncating\n", stderr);
+            break;
+        }
+        matrix[row][j++] = (int) strtol(split, &end, 10);
+        if (end == split) {
+            fputs("ERROR: not a valid integer input\n", stderr);
+            return -1;
+        }
+        split = strtok(NULL, " ");
+    }
+    return j;
+}
+
+int matrix_input(int rows, int cols, int matrix[rows][cols]) {
+    for (int i = 0; i < rows; ++i) {
+        print_row_prompt(i, cols);
+        char buf[INPUT_BUFFER_SIZE];
+        if (fgets(buf, INPUT_BUFFER_SIZE, stdin) == NULL) {
+            fputs("ERROR reading input\n", stderr);
+            return -1;
+        }
+        int elements_read = parse_row_input(buf, i, cols, matrix);
+        if (elements_read == -1) return -1;
+        if (elements_read < cols) {
+            fputs("ERROR: Input has less elements than required\n", stderr);
+            return -1;
+        }
+    }
+    return 0;
 }
 
 void matrix_add_33(int matrix_a[3][3], int matrix_b[3][3], int matrix_c[3][3]) {
@@ -39,12 +90,18 @@ void matrix_print_33(int matrix[3][3]) {
     matrix_print(3, 3, matrix);
 }
 
+int matrix_input_33(int matrix[3][3]) {
+    return matrix_input(3, 3, matrix);
+}
+
 int main(void) {
-    int matrix_a[3][3] = {{2, 3, 3}, {2, 1, 1}, {1, 3, 4}};
+    int matrix_a[3][3];
+    if (matrix_input_33(matrix_a) == -1) return -1;
     int matrix_b[3][3] = {{3, 1, 5}, {2, 2, 9}, {7, 3, 4}};
     int matrix_c[3][3];
     matrix_add_33(matrix_a, matrix_b, matrix_c);
     matrix_transpose_33(matrix_c);
+    putchar('\n');
     matrix_print_33(matrix_c);
     return 0;
 }
