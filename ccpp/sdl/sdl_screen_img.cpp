87cdd5447238b4d502f71e37e910266bc2e65055
Alex Rosegrid <rosegrid58@gmail.com>
2024-07-26 08:34:00 +0530

    .

--- ccpp/sdl/sdl_screen_img.cpp
+++ ccpp/sdl/sdl_screen_img.cpp
@@ -1,8 +1,8 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 420
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 500
 
 #define WINDOW_UP_HACK SDL_Event e; bool quit = false; while (!quit) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
 
@@ -22,11 +22,13 @@ int init(SDL_Window **window, SDL_Surface **screen_surface) {
     return 0;
 }
 
-int load_media(SDL_Surface **image_surface) {
-    *image_surface = SDL_LoadBMP("../herrschers.bmp");
+int load_media(SDL_Surface **image_surface, SDL_Surface *screen_surface) {
+    SDL_Surface *loaded_surface = SDL_LoadBMP("../herrschers.bmp");
+    *image_surface = SDL_ConvertSurface(loaded_surface, screen_surface->format, 0);
     if (*image_surface == NULL) {
         return -1;
     }
+    SDL_FreeSurface(loaded_surface);
     return 0;
 }
 
@@ -43,8 +45,13 @@ int main(int argc, char *argv[]) {
     SDL_Surface *screen_surface = NULL;
     SDL_Surface *image_surface = NULL;
     if (init(&window, &screen_surface) == 0) {
-        if (load_media(&image_surface) == 0) {
-            SDL_BlitSurface(image_surface, NULL, screen_surface, NULL);
+        if (load_media(&image_surface, screen_surface) == 0) {
+            SDL_Rect stretch_rect;
+            stretch_rect.x = 0;
+            stretch_rect.y = 0;
+            stretch_rect.w = SCREEN_WIDTH;
+            stretch_rect.h = SCREEN_HEIGHT;
+            SDL_BlitScaled(image_surface, NULL, screen_surface, &stretch_rect);
             SDL_UpdateWindowSurface(window);
             WINDOW_UP_HACK;
         }
