4047648b49df1721041e591b84a2c418d81ea685
Alex Rosegrid <rosegrid58@gmail.com>
2024-08-14 17:52:15 +0530

    .

--- ccpp/bisection.c
+++ ccpp/bisection.c
@@ -6,17 +6,17 @@ typedef struct {
     double b;
 } TwoTouple;
 
-void method_of_bisection(TwoTouple *const bounds, double (*const f)(const double), const int iter) {
+void method_of_bisection(const short precision, TwoTouple *const bounds, double (*const f)(const double), const int iter) {
     double c = (bounds->a + bounds->b) / 2;
     double error = (*f)(c);
-    printf("%d. [%.13lf, %.13lf] -> %.13lf\n", iter, bounds->a, bounds->b, error);
-    if (fabs(error) > 0.001) {
+    printf("%2d. [%.13lf, %.13lf] : %.13lf -> % .13lf\n", iter, bounds->a, bounds->b, c, error);
+    if (fabs(error) > pow(10, -precision)) {
         if (error > 0) {
             bounds->b = c;
         } else {
             bounds->a = c;
         }
-        method_of_bisection(bounds, f, iter + 1);
+        method_of_bisection(precision, bounds, f, iter + 1);
     }
 }
 
@@ -29,9 +29,14 @@ double g(const double x) {
 }
 
 int main(void) {
-    TwoTouple bounds_init = {
-        .a = 2.7, .b = 3
-    };
-    method_of_bisection(&bounds_init, f, 1);
+    TwoTouple bounds_init;
+    printf("Enter bound start: ");
+    scanf("%lf", &bounds_init.a);
+    printf("Enter bound end: ");
+    scanf("%lf", &bounds_init.b);
+    short precision;
+    printf("Enter number of decimal places: ");
+    scanf("%hd", &precision);
+    method_of_bisection(precision, &bounds_init, g, 1);
     return 0;
 }
