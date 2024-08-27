99681f62be1f27f575f3e6537a2afcfedab4c006
Alex Rosegrid <rosegrid58@gmail.com>
2024-08-27 19:24:25 +0530

    .

---
+++ ccpp/sparse_matrix.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    const size_t m;
+    int *data;
+} SqMatrix;
+
+void populate_matrix(SqMatrix *const matrix) {
+    for (size_t i = 0; i < matrix->m; ++i) {
+        for (size_t j = 0; j < matrix->m; ++j) {
+            printf("Enter element [%zu, %zu]: ", i, j);
+            size_t index = (matrix->m * i) + j;
+            scanf("%d", &matrix->data[index]);
+        }
+    }
+}
+
+void print_matrix(const SqMatrix *const matrix) {
+    for (size_t i = 0; i < matrix->m; ++i) {
+        for (size_t j = 0; j < matrix->m; ++j) {
+            size_t index = (matrix->m * i) + j;
+            printf("%d ", matrix->data[index]);
+        }
+        putchar('\n');
+    }
+}
+
+static inline int is_null_matrix(const SqMatrix *const matrix) {
+    for (size_t i = 0; i < (matrix->m * matrix->m); ++i) {
+        if (matrix->data[i]) return 0;
+    }
+    return 1;
+}
+
+static inline int is_diagonal_element(const size_t i, const size_t j) {
+    return (i == j);
+}
+
+static inline int is_auxilary_diagonal_element(const size_t i_max, const size_t i, const size_t j) {
+    if (i == 0) {
+        return (j == i + 1);
+    } else if (i == i_max) {
+        return (j == i - 1);
+    } else {
+        return (j == i - 1) || (j == i + 1);
+    }
+}
+
+static inline int is_non_zero_element(const SqMatrix *const matrix, const size_t index) {
+    return matrix->data[index];
+}
+
+int is_diagonal_matrix(const SqMatrix *const matrix) {
+    for (size_t i = 0; i < matrix->m; ++i) {
+        for (size_t j = 0; j < matrix->m; ++j) {
+            size_t index = (matrix->m * i) + j;
+            if (is_non_zero_element(matrix, index)) {
+                if (!is_diagonal_element(i, j)) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int is_tridiagonal_matrix(const SqMatrix *const matrix) {
+    for (size_t i = 0; i < matrix->m; ++i) {
+        for (size_t j = 0; j < matrix->m; ++j) {
+            size_t index = (matrix->m * i) + j;
+            if (is_non_zero_element(matrix, index)) {
+                if (!(is_diagonal_element(i, j) || is_auxilary_diagonal_element(matrix->m, i, j))) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int is_upper_triangular_matrix(const SqMatrix *const matrix) {
+    for (size_t i = 0; i < matrix->m; ++i) {
+        for (size_t j = 0; j < i; ++j) {
+            size_t index = (matrix->m * i) + j;
+            if (is_non_zero_element(matrix, index)) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int is_lower_triangular_matrix(const SqMatrix *const matrix) {
+    for (size_t i = 0; i < matrix->m; ++i) {
+        for (size_t j = i + 1; j < matrix->m; ++j) {
+            size_t index = (matrix->m * i) + j;
+            if (is_non_zero_element(matrix, index)) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+void print_irregular_matrix(const SqMatrix *const matrix) {
+    for (size_t i = 0; i < (matrix->m * matrix->m); ++i) {
+        if (is_non_zero_element(matrix, i)) {
+            printf("%d ", matrix->data[i]);
+        }
+    }
+    putchar('\n');
+}
+
+int main(void) {
+    // size_t m;
+    // printf("Enter matrix (square) dimention: ");
+    // scanf("%zu", &m);
+    const size_t m = 4;
+    int data[4 * 4] = {1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1};
+    SqMatrix matrix = {
+        .m = m,
+        // .data = malloc(m * m * sizeof(int))
+        .data = data
+    };
+    // populate_matrix(&matrix);
+    print_matrix(&matrix);
+    if (is_null_matrix(&matrix)) {
+        puts("This is a null matrix");
+    } else if (is_diagonal_matrix(&matrix)) {
+        puts("This is a diagonal matrix");
+    } else if (is_tridiagonal_matrix(&matrix)) {
+        puts("This is a tridiagonal matrix");
+    } else if (is_lower_triangular_matrix(&matrix)) {
+        puts("This is a lower triangular matrix");
+    } else if (is_upper_triangular_matrix(&matrix)) {
+        puts("This is an upper triangular matrix");
+    } else {
+        puts("This is an irregular matrix");
+        print_irregular_matrix(&matrix);
+    }
+    // free(matrix.data);
+    return 0;
+}
